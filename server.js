a9831f7c4b73dfe0868e41f13f26682a0440c92b
asingh57 <abhi_johnian@hotmail.com>
2019-02-24 20:24:27 -0500

    added mariadb module

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+
 var https = require('https');
 var mariadb = require('mariadb');
 var fs = require('fs');
@@ -5,12 +6,13 @@ var express = require('express');
 const app = express();
 const _ = require('lodash');
 
+
 const config = require('/config/config.json');//config stored outside repo for security reasons
 const defaultConfig = config.development;
 const environment = process.env.NODE_ENV || 'development';
 const environment_config = config[environment];
 const final_config = _.merge(defaultConfig, environment_config);
-
+const database_config=final_config.database;
 
 const ssl_details = {
   ca: fs.readFileSync(final_config.ssl_ca),
@@ -34,3 +36,16 @@ httpsServer.listen(final_config.node_port, () => {
 	console.log('HTTPS Server running on port '+final_config.node_port);
 });
 
+
+const serverCert = [fs.readFileSync(database_config.ssl_cert, "utf8")];// load the custom created server cert for mariadb
+
+mariadb
+ .createConnection({
+   host: database_config.host, 
+   ssl: {
+	 ca: serverCert
+   }, 
+   user: database_config.user, 
+   password:database_config.password, 
+   database:database_config.name
+ }).then(conn => {})
