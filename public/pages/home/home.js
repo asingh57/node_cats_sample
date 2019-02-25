9e612a2395f7789e5734b946fe1c08eed632f7b7
asingh57 <abhi_johnian@hotmail.com>
2019-02-25 01:13:26 -0500

    web app complete

--- public/pages/home/home.js
+++ public/pages/home/home.js
@@ -1,5 +1,5 @@
 
-function loadScript(url, callback)
+function loadScript(url, callback) //loads a static script file
 {
     // Adding the script tag to the head as suggested before
     var head = document.head;
@@ -16,25 +16,68 @@ function loadScript(url, callback)
     head.appendChild(script);
 }
 
+var escape = document.createElement('textarea');
+function escapeHTML(html) {//escapes data from html using a textarea conversion and back
+    escape.textContent = html;
+    return escape.innerHTML;
+}
 
-function add_table_row(v0,v1,v2,v3){
+
+function add_table_row(v0,v1,v2,v3){//add row to the preferences table
 	console.log("adding row")
-	$('#preferences_data_table tr.data_insert_row').before('<td>'+v0+'</td><td>'+v1+'</td><td>'+v2+'</td><td>'+v3+'</td>');
-	$('#preferences_data_table tr.data_insert_row').before('<td>'+v0+'</td><td>'+v1+'</td><td>'+v2+'</td><td>'+v3+'</td>');
-	$('#preferences_data_table tr.data_insert_row').before('<td>'+v0+'</td><td>'+v1+'</td><td>'+v2+'</td><td>'+v3+'</td>');
+	$('#preferences_data_table tr').eq(-1).before('<tr class="data_class"><td>'+v0+'</td><td>'+v1+'</td><td>'+v2+'</td><td colspan="3">'+v3+'</td><tr>');
 	console.log("added")
 }
-function fill_table(){
-	add_table_row("d","a","b","c");
-	showPage()
+
+
+function fill_table(){//fill up the preferences table
+    $('.data_class').remove()
+
+    $.get("/get_all_users", function(data, status){
+
+    for(i =0; i< data.length; i++){
+       add_table_row(escapeHTML(data[i].preference_id),escapeHTML(data[i].name),escapeHTML(data[i].colour),escapeHTML(data[i].animal));
+    }            
+       
+      }).fail(function(response) {
+        alert('Error: ' + response.responseText);
+        fill_table();
+    });
+	show_page();
 }
-function showPage() {
+
+
+
+function add_entry(){//add an entry to the preference database
+    hide_page();    
+    $.post("/add_user",{
+        name:$('#name').val(),
+        colour:$('#colour').val(),
+        animal:$("input[name='animal']:checked").val()
+        }
+    ,function(data,status){
+            location.reload();
+        
+    })
+    .fail(function(response) {
+        alert('Error: ' + response.responseText);
+        fill_table();
+    });
+}
+
+
+function show_page() {//unblock the page
   document.getElementById("loader").style.display = "none";
   document.getElementById("data_section").style.display = "block";
 }
 
+function hide_page() {//block the page till data loaded
+  document.getElementById("loader").style.display = "show";
+  document.getElementById("data_section").style.display = "none";
+}
 
-loadScript('/js/jquery.min.js',function(){
+
+loadScript('/js/jquery.min.js',function(){//load jquery library into the web page
 	$(document).ready(function () {
 	fill_table();	
 								}
--- server.js
+++ server.js
@@ -5,6 +5,7 @@ var fs = require('fs');
 var express = require('express');
 const app = express();
 const _ = require('lodash');
+const bodyParser = require("body-parser");
 
 
 const config = require('/config/config.json');//config stored outside repo for security reasons
@@ -12,25 +13,22 @@ const defaultConfig = config.development;
 const environment = process.env.NODE_ENV || 'development';
 const environment_config = config[environment];
 const final_config = _.merge(defaultConfig, environment_config);
-const database_config=final_config.database;
+const database_config=final_config.database; //config loaded for app
 
-const ssl_details = {
+const ssl_details = {//load ssl files
   ca: fs.readFileSync(final_config.ssl_ca),
   key: fs.readFileSync(final_config.ssl_key),
   cert: fs.readFileSync(final_config.ssl_cert)
 };
-app.use(express.static(__dirname + '/public'));
-
-//---------------
-//Home page scripts and HTML
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/public/pages/home/home.html');
-});
-//---------------
+app.use(express.static(__dirname + '/public'));//expose the public directory containing images and front end js
 
 
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 
-const httpsServer = https.createServer(ssl_details, app);
+
+const httpsServer = https.createServer(ssl_details, app);//setup https
 
 httpsServer.listen(final_config.node_port, () => {
 	console.log('HTTPS Server running on port '+final_config.node_port);
@@ -39,13 +37,93 @@ httpsServer.listen(final_config.node_port, () => {
 
 const serverCert = [fs.readFileSync(database_config.ssl_cert, "utf8")];// load the custom created server cert for mariadb
 
-mariadb
- .createConnection({
+var pool= mariadb
+ .createPool({
    host: database_config.host, 
    ssl: {
 	 ca: serverCert
    }, 
    user: database_config.user, 
    password:database_config.password, 
-   database:database_config.name
- }).then(conn => {})
+   database:database_config.name,
+   connectionLimit: 5
+ })
+
+
+
+//---------------
+//Home page HTML
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/public/pages/home/home.html');
+});
+//---------------
+
+
+app.get('/get_all_users', (req, res) => { //gets all users and their preferences
+    pool.getConnection()
+    .then(conn => {    
+      conn.query("Select * from preferences order by preference_id ASC;")
+        .then((rows) => {
+        conn.end();
+
+          res.send(rows);
+        })
+    }).catch(err => {
+      //not connected
+        conn.end();
+        console.log("connection failed");
+        res.status(500).send(err);
+    });
+});
+
+
+app.post('/add_user', (req, res) => {//inserts a new value into the preferencedb
+
+    pool.getConnection()
+    .then(conn => {    
+
+        req.body.name=req.body.name.trim()//trim whitespaces
+
+        if(//check param validity
+        req.body.name==="" 
+            || 
+        typeof req.body.name === undefined 
+            || 
+        req.body.name ===null 
+        ){
+            
+            var err="name is required";
+            res.status(500).send(err);
+            return;
+        }
+
+        if(
+        req.body.animal!=="cat" 
+            && 
+        req.body.animal !== "dog" 
+        ){
+            
+            var err="animal type is invalid";
+            res.status(500).send(err);
+            return;
+        }
+
+
+      conn.query("INSERT INTO preferences(name,colour,animal) VALUES(?,?,?);",
+        [req.body.name,req.body.colour,req.body.animal])
+        .then((data) => {        
+            conn.end();
+            res.send(data);
+        })
+    }).catch(err => {
+      //not connected
+        conn.end();
+        console.log("connection failed");
+        res.status(500).send(err);
+    });
+});
+
+
+
+
+
